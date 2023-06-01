dfe86d19a584e7a70b9563f3a357e5b73723ab6d
Albert Wu <albertwujj@gmail.com>
2023-05-31 17:21:02 -0700

    reset site to host Flux support page

--- _posts/medium-to-markdown.js
+++
@@ -1,6 +0,0 @@
-const mediumToMarkdown = require('medium-to-markdown');
-// Enter url here
-mediumToMarkdown.convertFromUrl('https://medium.com/@albertwu_14963/in-depth-explaining-openais-gpt-2-bc49b6fb8559')
-.then(function (markdown) {
-  console.log(markdown); //=> Markdown content of medium post
-});
